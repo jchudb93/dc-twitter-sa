2894404c30ca2db40df4f9bbbbb20486ce286964
Jose Chu <jchudb93@gmail.com>
2019-03-03 21:24:04 -0500

    word cloud

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -5,4 +5,12 @@ def remove_pattern(input_txt, pattern):
     for i in r:
         input_txt = re.sub(i, '', input_txt)
         
-    return input_txt    
+    return input_txt
+
+def hashtag_extract(x):
+    hashtags = []
+    for i in x:
+        ht = re.findall(r"#(\w+)", i)
+        hashtags.append(ht)
+
+    return hashtags
